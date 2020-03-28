d68ebb13b6aafc719c9699d528381871a742d237
yumauma20 <71yuma10@gmail.com>
2020-03-28 17:44:12 +0900

    会員登録画面とログイン画面の追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ Route::get('/','PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
 
 Route::resource('comments','CommentsController',['only' => ['store', 'edit', 'update', 'destroy']]);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
