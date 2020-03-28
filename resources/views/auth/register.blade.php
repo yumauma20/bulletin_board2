c241c654413377e67d12688d624ae7d471853566
yumauma20 <71yuma10@gmail.com>
2020-03-28 19:45:53 +0900

    ヘッダー部分のレイアウト変更と会員登録、ログイン時のリダイレクト先変更

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
