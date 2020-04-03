6855db88d858095ac9c60dad933dc4dbb0d0097b
yumauma20 <71yuma10@gmail.com>
2020-04-03 14:25:50 +0900

    herokuデプロイ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
